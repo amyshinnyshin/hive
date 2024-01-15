3f77c2f5d52a1e29d0162a089312a51539eb6618
amyshinnyshin@gmail.com <amyshinnyshin@gmail.com>
2024-01-15 15:53:58 -0600

    created my models, registered them and created superuser

--- hive/admin.py
+++ hive/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import JobApplication, Comment
 
 # Register your models here.
+admin.site.register(JobApplication)
+admin.site.register(Comment)
--- hive/models.py
+++ hive/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class JobApplication(models.Model): 
+    APPLIED = 'applied'
+    INTERVIEWS = 'interviews'
+    REJECTED = 'rejected'
+    OFFERED = 'offered'
+    DEFERRED = 'deferred'
+    SELECTED = 'selected'
+
+    STATUS_CHOICES = [
+        (APPLIED, 'Applied'), 
+        (INTERVIEWS, 'Interviews'), 
+        (REJECTED, 'Rejected'), 
+        (OFFERED, 'Offered'), 
+        (DEFERRED, 'Deferred'), 
+        (SELECTED, 'Selected'), 
+    ]
+    status = models.CharField(max_length = 20, choices = STATUS_CHOICES)
+    title = models.CharField(max_length = 100)
+    company_name = models.CharField(max_length = 100)
+    role = models.CharField(max_length = 100)
+    date_applied = models.DateField()
+    description = models.TextField()
+
+class Comment(models.Model):
+    job_application = models.ForeignKey(JobApplication, on_delete = models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
